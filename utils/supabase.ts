a67806a5d375e9f108912d3b75b93bd22974082a
KikeDeAlba <likingvirus1@gmail.com>
2024-02-03 21:48:56 -0700

    Add auto-refresh for Supabase authentication

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+import { AppState } from 'react-native'
 
 const supabaseUrl = process.env.SUPABASE_URL ?? ''
 const supabaseAnonKey = process.env.SUPABASE_KEY ?? ''
@@ -11,5 +12,15 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false
+  }
+})
+
+AppState.addEventListener('change', (state) => {
+  if (state === 'active') {
+    supabase.auth.startAutoRefresh()
+      .catch((error) => console.error('Error with auto-refresh', error))
+  } else {
+    supabase.auth.stopAutoRefresh()
+      .catch((error) => console.error('Error with auto-refresh', error))
   }
 })
